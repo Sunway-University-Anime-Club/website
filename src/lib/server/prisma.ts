f0e0615d4e43a74232cb9e7deb5896f17d2f65b1
Spimy <officialspyme@gmail.com>
2023-11-06 00:42:04 +0800

    👌 IMPROVE: Use mongoose instead of prisma

--- src/lib/server/prisma.ts
+++
@@ -1,7 +0,0 @@
-import { dev } from '$app/environment';
-import { PrismaClient } from '@prisma/client/edge';
-
-const prisma = global.prisma || new PrismaClient();
-if (dev) global.prisma = prisma;
-
-export { prisma };
