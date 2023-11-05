893d6034048dc3ecc32bf8760733794af73c9a4a
Spimy <officialspyme@gmail.com>
2023-11-06 00:42:36 +0800

    👌 IMPROVE: Update type declarations

--- src/app.d.ts
+++ src/app.d.ts
@@ -6,12 +6,25 @@ import type { PrismaClient } from '@prisma/client';
 declare global {
 	namespace App {
 		// interface Error {}
-		// interface Locals {}
+		interface Locals {
+			auth: import('lucia').AuthRequest;
+			validate: import('@lucia-auth/sveltekit').Validate;
+			validateUser: import('@lucia-auth/sveltekit').ValidateUser;
+			setSession: import('@lucia-auth/sveltekit').SetSession;
+		}
 		// interface PageData {}
 		// interface Platform {}
 	}
 	// eslint-disable-next-line no-var
 	var prisma: PrismaClient;
+
+	declare namespace Lucia {
+		type Auth = import('$lib/server/lucia').Auth;
+		type DatabaseUserAttributes = {
+			username: string;
+		};
+		// type DatabaseSessionAttributes = {};
+	}
 }
 
 export {};
