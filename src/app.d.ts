eba6845e3235551aa2ded4e260c06e9e9bff3f11
Spimy <officialspyme@gmail.com>
2023-11-06 15:57:08 +0800

    👌 IMPROVE: Standardise type imports

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,13 +1,14 @@
 // See https://kit.svelte.dev/docs/types#app
 
 import type { UserRole } from '$lib/schema/user.schema';
+import type { AuthRequest } from 'lucia';
 
 // for information about these interfaces
 declare global {
 	namespace App {
 		// interface Error {}
 		interface Locals {
-			auth: import('lucia').AuthRequest;
+			auth: AuthRequest;
 		}
 		// interface PageData {}
 		// interface Platform {}
