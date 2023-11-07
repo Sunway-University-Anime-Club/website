84729e836e50ca177ae46e92d27bb908a01a15f7
Spimy <officialspyme@gmail.com>
2023-11-07 15:31:56 +0800

    👌 IMPROVE: Disallow access to logout page for all users

--- src/routes/logout/+page.server.ts
+++ src/routes/logout/+page.server.ts
@@ -1,5 +1,10 @@
 import { auth as lucia } from '$lib/server/lucia';
 import { redirect, type Actions } from '@sveltejs/kit';
+import type { PageServerLoad } from './$types';
+
+export const load: PageServerLoad = async () => {
+	throw redirect(302, '/');
+};
 
 export const actions: Actions = {
 	default: async (event) => {
