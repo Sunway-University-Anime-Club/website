4b9fd7a2368374e23645b2434edac9b886fe9a2c
Spimy <officialspyme@gmail.com>
2023-11-07 15:29:54 +0800

    👌 IMPROVE: Add logout button

--- src/routes/admin/+page.svelte
+++ src/routes/admin/+page.svelte
@@ -1,1 +1,8 @@
+<script lang="ts">
+	import { enhance } from '$app/forms';
+</script>
+
 <h1>Admin</h1>
+<form method="POST" use:enhance>
+	<button type="submit" formaction="/logout">Logout</button>
+</form>
